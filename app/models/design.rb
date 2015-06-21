3c89dfce4110ae876b883778fd89dc0aab8eef94
Mike Nguyen <mike@toch.im>
2015-06-22 06:42:05 +0700

    Upload file size less than 5mb

--- app/models/design.rb
+++ app/models/design.rb
@@ -4,4 +4,5 @@ class Design < ActiveRecord::Base
   # TODO Review hole size in ten dollar bill note first
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_size :image, :less_than => 5.megabytes
 end
