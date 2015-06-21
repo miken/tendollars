617bf033d0081f13fd3f6ffb10455663767cc97d
Mike Nguyen <mike@toch.im>
2015-06-21 09:56:06 +0700

    Add new, create & show to design

--- app/controllers/designs_controller.rb
+++ app/controllers/designs_controller.rb
@@ -1,4 +1,20 @@
 class DesignsController < ApplicationController
-  def new
+  def show
+    @design = Design.find params[:id]
   end
+
+  def create
+    @design = Design.new design_params
+    @design.save
+    redirect_to @design
+  end
+
+  def new
+    @design = Design.new
+  end
+
+  private
+    def design_params
+      params.require(:design).permit(:name, :description, :image)
+    end
 end
