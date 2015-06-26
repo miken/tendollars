370db011dba84bbe81554f188c79d94046547d54
Mike Nguyen <mike@toch.im>
2015-06-26 14:21:13 +0700

    Layout improvement

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @no_container = true
   end
 end
--- app/models/design.rb
+++ app/models/design.rb
@@ -7,7 +7,7 @@ class Design < ActiveRecord::Base
       :medium => "x300"
       },
     :convert_options => {
-      :medium => "-gravity center -extent 708x300+75 '#{Rails.root.join('public/old_ten_dollars.png')}' -composite"
+      :medium => "-gravity center -extent 708x300+60 '#{Rails.root.join('public/old_ten_dollars.png')}' -composite"
     }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates_attachment_size :image, :less_than => 5.megabytes
